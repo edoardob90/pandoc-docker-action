cde7bce7359c1a5dab18a7bc294544fec6aabc2b
edoardob90 <edoardob90@gmail.com>
2022-11-27 19:06:15 +0100

    Update action.yml and entrypoint script (simpler)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,18 @@
 #!/bin/bash -l
 
-year=$(date +%Y)
-day=$(date +%d)
+dest=${1}
+year=${2:-$(date +%Y)}
+day=${3:-$(date +%d)}
 
 mkdir -p "$day"
-
-dest=${day}/${1:-prompt_${day}.md}
 
 /usr/local/bin/pandoc -f html -t gfm --wrap=preserve "https://adventofcode.com/$year/day/$day" | \
     awk '{
         if ($0 ~ /<div role="main">/) {
             getline
             while ($0 !~ /^To play/) {
-                if ($0 ~ /^#+/) {
-                    title=substr($0, index($0, "Day"))
-                    sub(/---/, "", title)
-                    printf("# %s\n", title)
-                } else print $0
+                print $0
                 getline
             }
         }
-    }' > $dest
+    }' > $day/$dest
